370f4851f609516747f769bd8da58c8736cf2484
jimmabelle <jimmabelle@users.noreply.github.com>
2017-11-24 14:03:53 +0100

    creating js

---
+++ script.js
@@ -0,0 +1,11 @@
+const openWindow = (window) => {
+  //toggle openPage that has class openPage to .expand
+  //get correct html file by using date
+  //sliding animation
+  console.log(this.id)
+}
+
+//document.getElementsByClassName("window").onclick = openWindow
+
+document.getElementsByClassName("window").onclick
+console.log(document.getElementsByClassName("window"))

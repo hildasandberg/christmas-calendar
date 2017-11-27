e261e921f668ef5675363eaffe196fb4f06cf4c5
Hilda <hilda.sandberg@gmail.com>
2017-11-27 14:39:15 +0100

    merge

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-const openWindow = (lucka) => {
-  //toggle openPage that has class openPage to .expand
-  //get correct html file by using date
-  //sliding animation
-  // console.log("Hej " + lucka)
-  document.getElementById("toprow").classList.toggle("expand3")
-  document.getElementById("openPage").classList.toggle("expand1")
-  document.getElementById("bottomrow").classList.toggle("expand2")
-}
-
-
-const elements = [...document.getElementsByClassName("window")]; // Just to convert the NodeList to an Array to gain acceess to .forEach
-
+//Open window on click
+// Just to convert the NodeList to an Array to gain acceess to .forEach
+const elements = [...document.getElementsByClassName("window")];
 // gets the element and the event
 elements.forEach((element) => {
   element.onclick = (event) => {
-    sayHello();
     // Gets the element id
     // console.log('HELLO!', element.id);
     // Do stuff with the window here
--- utils.js
+++ utils.js
@@ -1,13 +1,11 @@
-const sayHello= () => {
-  console.log('hello');
-}
-
+//Open webpage renderer by toggling classes.
 const openWindow = (lucka) => {
   //toggle openPage that has class openPage to .expand
-  //get correct html file by using date
-  //sliding animation
-  // console.log("Hej " + lucka)
   document.getElementById("toprow").classList.toggle("expand3")
   document.getElementById("openPage").classList.toggle("expand1")
   document.getElementById("bottomrow").classList.toggle("expand2")
+  //get correct html file by using date
+  console.log("Hej " + lucka)
+  //sliding animation
+
 }

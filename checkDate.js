22f27101140c7c534b92ba34501541f59a8349f2
Hilda <hilda.sandberg@gmail.com>
2017-12-01 14:40:18 +0100

    changed the dates to correct dates

--- checkDate.js
+++ checkDate.js
@@ -1,6 +1,6 @@
 //use new Date() to enerate a new Date object containing the the current date and time
 const checkDate = (lucka) => {
-  const today = new Date("2017-12-17") //Remove date to get todays date
+  const today = new Date() //Remove date to get todays date
   const luckaDate = new Date(`${lucka} 2017`)
 
   // alert(`Luckans datum ${luckaDate}`)
